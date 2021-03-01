525221257ebb5c134945fef1fe5c9a8e09a71f57
skgilmore <skg.gilmore@gmail.com>
2021-03-01 09:56:31 -0600

    place names from js to csharp

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,16 @@
 // Put your code here
+const placeNames = ["Nashville", "Hong Kong", "The back yard", "Earth", "London", "The mall", "Ryman Auditorium", "The Great Wall of China",]
+
+console.log(placeNames, "All the place names")
+
+placeNames.forEach(element => console.log(element))
+
+
+function filterItems(placeNames, query) {
+    return placeNames.filter(function (el) {
+        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+    })
+}
+
+
+console.log(filterItems(placeNames, "the"), "stars with the")
